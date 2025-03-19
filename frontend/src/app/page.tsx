10b6169276fbdb9c969a6408c6011c2184b950bd
lassana <lassana.dansoko@gmail.com>
2025-03-19 17:15:38 +0100

    page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
-    <h1>🚀 Déploiement Automatique Testé  ta race ! ✅</h1>
-    <p>Si vous  ce , GitHub Actions fonctionne bienddddreee! 🎉</p>
+    <h1>🚀 Déploiement Automatique Testé ! ✅</h1>
+    <p>test du deploiement auto! 🎉</p>
   </div>
   );
 }
